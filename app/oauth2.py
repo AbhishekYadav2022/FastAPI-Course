691f42d21bfa86d654ce1b685ad4111edfc38dfc
AbhishekYadav2022 <106291898+AbhishekYadav2022@users.noreply.github.com>
2022-12-16 18:18:45 +0530

    Environment Variables Added

--- app/oauth2.py
+++ app/oauth2.py
@@ -4,6 +4,7 @@ from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -11,9 +12,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 # Algorithm
 # Expiration Time 
 
-SECRET_KEY = "04hd83jfkdjgurk94jfekdhdk4763d8f9hhdk4hfk"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 def create_access_token(data: dict):
     to_encode = data.copy()
